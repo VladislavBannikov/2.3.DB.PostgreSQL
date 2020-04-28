0df118fd01452eeb5225874d7714b3e2ccf7aa36
zuko1623 <zuko1623@gmail.com>
2020-04-28 10:18:03 +0300

    parameter substitution fix

--- main.py
+++ main.py
@@ -29,6 +29,7 @@ def create_db(): # создает таблицы
     with conn.cursor() as cur:
         for table_name in reversed(list(queries.keys())):       # DROP all tables
             cur.execute(f'DROP TABLE if exists "{table_name}";')
+            # cur.execute("DROP TABLE if exists %s ", (table_name, ))             # <<<<<<< why this statement doesn't work???
         for q in queries.values():                              # CREATE all tables
             cur.execute(q)
 
@@ -38,9 +39,11 @@ def create_db(): # создает таблицы
 
 def get_students(course_id): # возвращает студентов определенного курса
     with conn.cursor() as cur:
-        q = f"select st.name, st.gpa, st.birth from student as st \
-                join course_student as cs on cs.id_student = st.id where cs.id_course = '{course_id}'"
-        cur.execute(q)
+        # q = f"select st.name, st.gpa, st.birth from student as st \
+        #         join course_student as cs on cs.id_student = st.id where cs.id_course = '{course_id}'"
+        q = "select st.name, st.gpa, st.birth from student as st \
+                       join course_student as cs on cs.id_student = st.id where cs.id_course = %s"
+        cur.execute(q, (course_id, ))
         return cur.fetchall()
 
 
@@ -50,9 +53,12 @@ def add_students(course_id, students): # создает студентов и з
             name = st.get('name')
             gpa = st.get('gpa')
             birth = st.get('birth')
-            cur.execute(f"INSERT INTO student(name,gpa,birth) VALUES ('{name}','{gpa}',to_timestamp('{birth}','YYYY-MM-DD')) RETURNING id")
+            # cur.execute(f"INSERT INTO student(name,gpa,birth) VALUES ('{name}','{gpa}',to_timestamp('{birth}','YYYY-MM-DD')) RETURNING id")
+            cur.execute(
+                "INSERT INTO student(name,gpa,birth) VALUES (%s,%s,to_timestamp(%s,'YYYY-MM-DD')) RETURNING id", (name, gpa, birth))
             st_id = cur.fetchone()[0]
-            cur.execute(f"INSERT INTO course_student(id_student, id_course) VALUES ('{st_id}','{course_id}')")
+            # cur.execute(f"INSERT INTO course_student(id_student, id_course) VALUES ('{st_id}','{course_id}')")
+            cur.execute("INSERT INTO course_student(id_student, id_course) VALUES (%s,%s)", (st_id, course_id))
     conn.commit()
 
 
@@ -61,16 +67,21 @@ def add_student(st): # просто создает студента
         name = st.get('name')
         gpa = st.get('gpa')
         birth = st.get('birth')
-        cur.execute(f"INSERT INTO student(name,gpa,birth) VALUES ('{name}','{gpa}',to_timestamp('{birth}','YYYY-MM-DD')) RETURNING id")
+        # cur.execute(f"INSERT INTO student(name,gpa,birth) VALUES ('{name}','{gpa}',to_timestamp('{birth}','YYYY-MM-DD')) RETURNING id")
+        cur.execute(
+            "INSERT INTO student(name,gpa,birth) VALUES (%s,%s,to_timestamp(%s,'YYYY-MM-DD')) RETURNING id", (name, gpa, birth))
     conn.commit()
 
 
 def get_student(student_id):
     with conn.cursor() as cur:
-        q = f"select st.name, st.gpa, st.birth, c.name from student as st \
-                join course_student as cs on cs.id_student = st.id \
-                join course as c on c.id = cs.id_course where st.id = '{student_id}'"
-        cur.execute(q)
+        # q = f"select st.name, st.gpa, st.birth, c.name from student as st \
+        #         join course_student as cs on cs.id_student = st.id \
+        #         join course as c on c.id = cs.id_course where st.id = '{student_id}'"
+        q = "select st.name, st.gpa, st.birth, c.name from student as st \
+                        join course_student as cs on cs.id_student = st.id \
+                        join course as c on c.id = cs.id_course where st.id = %s"
+        cur.execute(q, (student_id, ))
         return cur.fetchall()
 
 
